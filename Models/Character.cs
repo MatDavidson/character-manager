2f8092ecabb0d994a08b0c1a3d09c07c095a2e6f
Mat Davidson <matdavidson88@gmail.com>
2021-03-18 18:43:12 -0400

    Added some form validation for level and abilites. Added drop downs for race and class

--- Models/Character.cs
+++ Models/Character.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,18 +13,63 @@ namespace CharacterManager.Models
         public string Name { get; set; }
         public string Race { get; set; }
         public string Class { get; set; }
+
+        [Range(1, 20)]
         public int Level { get; set; }
         public int RolledHP { get; set; }
+
+        [Range(1, 20)]
+        [Display(Name = "Strength")]
         public int Str { get; set; }
+
+        [Range(1, 20)]
+        [Display(Name = "Constitution")]
         public int Con { get; set; }
+
+        [Range(1, 20)]
+        [Display(Name = "Dexterity")]
         public int Dex { get; set; }
+
+        [Range(1, 20)]
+        [Display(Name = "Intelligence")]
         public int Int { get; set; } //Intelligence 
+
+        [Range(1, 20)]
+        [Display(Name = "Wisdom")]
         public int Wis { get; set; }
+
+        [Range(1, 20)]
+        [Display(Name = "Charisma")]
         public int Cha { get; set; }
 
-        public int TotalHP()
-        {
-            return (Level * Con) + RolledHP;
-        }
+
+    }
+
+    public enum Class
+    {
+        Barbarian,
+        Bard,
+        Cleric,
+        Druid,
+        Fighter,
+        Monk,
+        Paladin,
+        Ranger,
+        Rogue,
+        Sorcerer,
+        Warlock,
+        Wizard
+    }
+
+    public enum Race
+    {
+        Dragonborn,
+        Dwarf,
+        Elf,
+        Gnome,
+        HalfElf,
+        HalfOrc,
+        Human,
+        Tiefling
     }
 }
